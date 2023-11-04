a0695e247da75b32700d538c44100716b4a136ef
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2023-11-04 13:17:11 +0400

    EditHelicopter

---
+++ Assets/Scripts/Enemy/BladesMover.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladesMover : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(0, 5, 0);
+    }
+}
--- Assets/Scripts/Enemy/StateMachine/States/DieState.cs
+++ Assets/Scripts/Enemy/StateMachine/States/DieState.cs
@@ -7,6 +7,7 @@ public class DieState : State
     [SerializeField] private EnemyAnimations _enemyAnimations;
     [SerializeField] private Effect _effect;
     [SerializeField] private KilledInfo _killedInfo;
+    [SerializeField] private bool _isHelicopter = false;
 
     private WaitForSeconds _waitForSeconds = new WaitForSeconds(3f);
 
@@ -17,6 +18,9 @@ public class DieState : State
 
     private IEnumerator Die()
     {
+        if (_isHelicopter)
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+
         _killedInfo.ChangeValue();
         _effect.PlayEffect();
         _enemyAnimations.Die(true);
