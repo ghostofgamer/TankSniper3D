0a542552030c8b6e7e6b3913705e3edb67dd1429
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2023-11-05 18:28:59 +0400

    AddBulletsOther

--- Assets/Scripts/Enemy/EnemyShoot.cs
+++ Assets/Scripts/Enemy/EnemyShoot.cs
@@ -9,6 +9,17 @@ public class EnemyShoot : MonoBehaviour
     [SerializeField] private Transform _container;
     [SerializeField] private float _delay;
 
+    private readonly int _ammoCount = 10;
+
+    private ObjectPool<Bullet> _pool;
+    private bool _autoExpand = true;
+
+    private void Start()
+    {
+        _pool = new ObjectPool<Bullet>(_prefab, _ammoCount, _container);
+        _pool.GetAutoExpand(_autoExpand);
+    }
+
     public IEnumerator Shoot()
     {
         WaitForSeconds _waitForSeconds = new WaitForSeconds(_delay);
@@ -16,9 +27,18 @@ public class EnemyShoot : MonoBehaviour
         while (true)
         {
             yield return _waitForSeconds;
-            Bullet bullet = Instantiate(_prefab, _container);
-            bullet.Init(_shootPosition);
+            Shooting();
+            //Bullet bullet = Instantiate(_prefab, _container);
+            //bullet.Init(_shootPosition);
             //yield return _waitForSeconds;
+        }
+    }
+
+    private void Shooting()
+    {
+        if (_pool.TryGetObject(out Bullet bullet, _prefab))
+        {
+            bullet.Init(_shootPosition);
         }
     }
 }
