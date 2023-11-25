ee4cefe93b3ede336e1c16612f7e0d932fd3c199
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2023-11-25 16:09:57 +0400

    AddMobileApplicationChecker

---
+++ Assets/Scripts/MobileChecker.cs
@@ -0,0 +1,25 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileChecker : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private CinemachineVirtualCamera _cinemachineVirtualCamera;
+    [SerializeField] private float _fov;
+
+    private void Awake()
+    {
+        if (Application.isMobilePlatform)
+        {
+            if (_cinemachineVirtualCamera != null)
+                _cinemachineVirtualCamera.m_Lens.FieldOfView = _fov;
+
+            if (_camera != null)
+                _camera.fieldOfView = _fov;
+        }
+        else
+            return;
+    }
+}
