ec5d4079ba928ab2178a70cc73c52c28b08a493f
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2023-11-27 19:41:50 +0400

    EditZoomAimCamera

--- Assets/Scripts/Test/FovMoveAim.cs
+++ Assets/Scripts/Test/FovMoveAim.cs
@@ -6,13 +6,17 @@ using UnityEngine.UI;
 public class FovMoveAim : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+    [SerializeField] private float _fovTarget;
+    [SerializeField] private float _fovStart;
+
+    private float _speed = 1.65f;
 
     private void Update()
     {
         if (_camera.enabled)
-            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, 30, 1.65f * Time.deltaTime);
+            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, _fovTarget, _speed * Time.deltaTime);
         
         else
-            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, 60, 1.65f * Time.deltaTime);
+            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, _fovStart, _speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Test/ResetGame.cs
+++ Assets/Scripts/Test/ResetGame.cs
@@ -7,7 +7,8 @@ public class ResetGame : AbstractButton
     [SerializeField] private Save _save;
     [SerializeField] private Load _load;
     [SerializeField] private Storage _storage;
-public override void OnClick()
+
+    public override void OnClick()
     {
         _save.Reset();
         _load.ResetLoad();
