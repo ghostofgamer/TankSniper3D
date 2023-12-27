ff8f8bf667adb48dd594e4d3ac9e9d5d69f712d9
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2023-12-27 12:38:43 +0400

    EditPointerProcentScreen

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -30,15 +30,29 @@ public class Pointer : MonoBehaviour
             _pointerPosition = Camera.main.WorldToScreenPoint(_target.position);
             //_imageCurrent.transform.position = Camera.main.WorldToScreenPoint(_target.position);
             _imageCurrent.transform.position = _pointerPosition;
+            int width = Screen.width;
+            int height = Screen.height;
+            Vector2 center = new Vector2(width / 2, height / 2);
+            Debug.Log(center);
+            int procentX = (width / 100) * 8;
+            int procentY = (height / 100) * 15;
+            Debug.Log(procentX);
+            int minX = width / 2 - procentX;
+            int maxX = width/2  + procentX;
+            int minY = height / 2 - procentY;
+            int maxY = height / 2 + procentY;
+            //Debug.Log("X " + X);
+            Debug.Log("PointerPosition " + _pointerPosition.x);
+            Debug.Log("AIM " + _aim.transform.position.x);
 
-            _pointerPosition.x = Mathf.Clamp(_pointerPosition.x, /*Screen.width+ _range*/_aim.transform.position.x - 145f /*ProcentLeft*/, /*ProcentRight*/ _aim.transform.position.x + 145f /*Screen.width - _range*/);
-            _pointerPosition.y = Mathf.Clamp(_pointerPosition.y, _aim.transform.position.y - 145f, _aim.transform.position.y + 145f/*Screen.height - 55f*/);
+            _pointerPosition.x = Mathf.Clamp(_pointerPosition.x,/*_aim.transform.position.x - 145f*/minX, maxX/* _aim.transform.position.x + 145f*/ /*Screen.width - _range*/);
+            _pointerPosition.y = Mathf.Clamp(_pointerPosition.y, minY /*_aim.transform.position.y - 145f*/, maxY/*_aim.transform.position.y + 145f*//*Screen.height - 55f*/);
 
             //_image.transform.position = Vector3.Lerp(_image.transform.position, _pointerPosition, 6 * Time.deltaTime);
             _image.transform.position = Vector3.Lerp(_image.transform.position, _pointerPosition, 6 * Time.deltaTime);
             _image.transform.LookAt(_imageCurrent.transform);
 
-            if(Vector3.Distance(_image.transform.position, _imageCurrent.transform.position) < 90)
+            if (Vector3.Distance(_image.transform.position, _imageCurrent.transform.position) < 36)
             {
                 _newImage.gameObject.SetActive(false);
             }
